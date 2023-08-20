2fcd9123fb12bcfd98e46d3336ef944751e5db39
AllaphyUFS <allaphy@academico.ufs.br>
2023-08-20 16:28:47 -0300

    feat: adding bibliotecario request and response models

---
+++ Biblioteca.Domain/Models/Requests/AddBibliotecarioRequest.cs
@@ -0,0 +1,29 @@
+﻿using Biblioteca.Domain.Entities;
+
+namespace Biblioteca.Domain.Models.Requests
+{
+    public class AddBibliotecarioRequest
+    {
+        public string Nome { get; private set; }
+        public string CPF { get; private set; }
+        public string Email { get; private set; }
+        public string Senha { get; private set; }
+        public DateTime DataCadastro { get; private set; }
+        public virtual Endereco? Endereco { get; private set; }
+
+        public AddBibliotecarioRequest(string nome,
+            string cpf,
+            string email,
+            string senha,
+            DateTime dataCadastro,
+            Endereco? endereco)
+        {
+            Nome = nome;
+            CPF = cpf;
+            Email = email;
+            Senha = senha;
+            DataCadastro = dataCadastro;
+            Endereco = endereco;
+        }
+    }
+}
