ac55e98a0696036ba44acaee25e7f019b80028a4
AllaphyUFS <allaphy@academico.ufs.br>
2023-08-20 16:29:27 -0300

    fix: removing connection string

--- Biblioteca.Infrastructure/InfrastructureDI.cs
+++ Biblioteca.Infrastructure/InfrastructureDI.cs
@@ -11,7 +11,7 @@ namespace Biblioteca.Infrastructure
        public static void AddRepositories(IServiceCollection services)
         {
             // Database service
-            string? connectionString = "Server=66.94.117.155; Port=3306; Database=es2_biblioteca; Uid=es2; Pwd=5KeWM58tNiSSEh2C;";//Environment.GetEnvironmentVariable("BIBLIOTECA_CONNECTION_STRING");
+            string? connectionString = Environment.GetEnvironmentVariable("BIBLIOTECA_CONNECTION_STRING");
             services.AddDbContext<InfraDbContext>(options =>
             {
                 options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
