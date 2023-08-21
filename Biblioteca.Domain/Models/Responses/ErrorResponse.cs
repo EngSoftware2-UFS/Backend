313dcca5ed27cd3e0681c51a7e909b60c5fa0fce
AllaphyUFS <allaphy@academico.ufs.br>
2023-08-21 00:11:33 -0300

    feat: adding mapped responses

---
+++ Biblioteca.Domain/Models/Responses/ErrorResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace Biblioteca.Application.Models.Responses
+{
+    public class ErrorResponse
+    {
+        public List<string> Errors { get; set; }
+        public string? StackTrace { get; set; }
+    }
+}
