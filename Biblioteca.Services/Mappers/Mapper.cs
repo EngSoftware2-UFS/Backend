92b04aa600f060732d2183090ee7e8eb45197230
AllaphyUFS <allaphy@academico.ufs.br>
2023-09-02 22:59:31 -0300

    refactor: renaming class mapper name

--- Biblioteca.Services/Mappers/Mapper.cs
+++ Biblioteca.Services/Mappers/Mapper.cs
@@ -1,14 +1,16 @@
 ﻿using AutoMapper;
 using Biblioteca.Domain.Entities;
+using Biblioteca.Domain.Models;
+using Biblioteca.Domain.Enums;
 using Biblioteca.Domain.Models.Requests;
 using Biblioteca.Domain.Models.Responses;
 using Biblioteca.Domain.Views;
 
 namespace Biblioteca.Services.Mappers
 {
-    public class FuncionarioMapper : Profile
+    public class Mapper : Profile
     {
-        public FuncionarioMapper()
+        public Mapper()
         {
             // Atendente
             CreateMap<Atendente, AtendenteResponse>();
@@ -24,6 +26,18 @@ namespace Biblioteca.Services.Mappers
 
             CreateMap<Emprestimo, AtendenteEmprestimoResponse>();
             CreateMap<ReservasView, HistoricoReservas>();
+
+            CreateMap<Atendente, UserData>()
+                .ConstructUsing(atendente => 
+                    new UserData(atendente.Id, atendente.Nome, atendente.Email, ETipoUsuario.ATENDENTE));
+
+            CreateMap<Cliente, UserData>()
+                .ConstructUsing(cliente =>
+                    new UserData(cliente.Id, cliente.Nome, cliente.Email, ETipoUsuario.CLIENTE));
+
+            CreateMap<Bibliotecario, UserData>()
+                .ConstructUsing(bibliotecario =>
+                    new UserData(bibliotecario.Id, bibliotecario.Nome, bibliotecario.Email, ETipoUsuario.BIBLIOTECARIO));
         }
     }
 }
