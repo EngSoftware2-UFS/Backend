fd843ba3a9f5d52803403de285733f94d52e2b76
AllaphyUFS <allaphy@academico.ufs.br>
2023-09-14 13:04:44 -0300

    fix: adding ObraId and some functions for emprestimo

--- Biblioteca.Domain/Models/Responses/EmprestimoResponse.cs
+++ Biblioteca.Domain/Models/Responses/EmprestimoResponse.cs
@@ -1,4 +1,5 @@
-﻿using Biblioteca.Domain.Views;
+﻿using Biblioteca.Domain.Entities;
+using Biblioteca.Domain.Views;
 
 namespace Biblioteca.Domain.Models.Responses
 {
@@ -11,7 +12,9 @@ namespace Biblioteca.Domain.Models.Responses
         public int QuantidadeRenovacao { get; private set; }
         public bool Inadimplencia { get; private set; }
         public string Status { get; private set; } = "";
-        public List<string> Obras { get; private set; } = new List<string>();
+        public List<ObraShortResponse> Obras { get; private set; } = new List<ObraShortResponse>();
+        public ulong? ClienteId { get; private set; } = null;
+        public double? Multa { get; private set; } = null;
 
         public EmprestimoResponse() { }
 
@@ -42,12 +45,53 @@ namespace Biblioteca.Domain.Models.Responses
             QuantidadeRenovacao = emprestimoView.QuantidadeRenovacao;
             Inadimplencia = emprestimoView.Inadimplencia;
             Status = emprestimoView.Status;
-            addObra(emprestimoView.Titulo);
+            addObra(emprestimoView.ObraId, emprestimoView.Titulo);
+            if (verificaInadimplencia())
+                Multa = calcularMulta();
         }
 
-        public void addObra(string titulo)
+        public void addObra(ulong obraId, string titulo)
         {
-            Obras.Add(titulo);
+            Obras.Add(new ObraShortResponse(obraId, titulo));
+        }
+
+        public void setClienteId(ulong clienteId)
+        {
+            ClienteId = clienteId;
+        }
+
+        public void renovarEmprestimo()
+        {
+            if (QuantidadeRenovacao > 0)
+            {
+                QuantidadeRenovacao -= 1;
+                DataDevolucao = DataDevolucao.HasValue ? DataDevolucao.Value.AddDays(7) : null;
+            }
+            else throw new OperationCanceledException("Não é possível fazer mais renovações para esse empréstimo.");
+        }
+
+        public double calcularMulta()
+        {
+            const double valorMultaPorDia = 1.00;
+            if (verificaInadimplencia() && DataDevolucao.HasValue)
+            {
+                var diasUltrapassados = (DataDevolucao.Value - DateTime.UtcNow).TotalDays;
+                double multa = diasUltrapassados * valorMultaPorDia;
+                return multa;
+            }
+
+            return 0;
+        }
+
+        private bool verificaInadimplencia()
+        {
+            if (DataDevolucao.HasValue && DataDevolucao.Value > DateTime.UtcNow)
+            {
+                if (!Inadimplencia) Inadimplencia = true;
+                return true;
+            }
+
+            return false;
         }
     }
 }
