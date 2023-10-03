94bf698997a4ffaafcc2f06a0b5e0d4d28e2efcb
AllaphyUFS <allaphy@academico.ufs.br>
2023-10-03 02:55:52 -0300

    feat: adding background service to update reservas and make exemplar available if expired, and seting inadimplencia for emprestimos.

---
+++ Biblioteca.Services/Services/Rotinas.cs
@@ -0,0 +1,33 @@
+﻿using Biblioteca.Infrastructure.Repositories.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Biblioteca.Services.Services
+{
+    public class Rotinas : BackgroundService
+    {
+        private readonly IServiceScopeFactory _factory;
+        private readonly TimeSpan _period = TimeSpan.FromHours(1);
+
+        public Rotinas(
+            IServiceScopeFactory factory )
+        {
+            _factory = factory;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            using PeriodicTimer timer = new PeriodicTimer(_period);
+            while (
+                !stoppingToken.IsCancellationRequested &&
+                await timer.WaitForNextTickAsync(stoppingToken))
+            {
+                await using AsyncServiceScope asyncScope = _factory.CreateAsyncScope();
+                var _reservaRepository = asyncScope.ServiceProvider.GetRequiredService<IReservaRepository>();
+                var _emprestimoRepository = asyncScope.ServiceProvider.GetRequiredService<IEmprestimoRepository>();
+                _reservaRepository.VerifyReservas();
+                _emprestimoRepository.VerifyInadimplencia();
+            }
+        }
+    }
+}
